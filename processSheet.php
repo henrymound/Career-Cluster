fda8217813c65a5cdf85b5ec52a5e13e92b1c846
Henry Mound <henrymound@gmail.com>
2015-04-17 19:29:28 +0000

    added PHP file to read CSV form

--- processSheet.php
+++ processSheet.php
@@ -1,5 +1,19 @@
-<php 
-https://docs.google.com/spreadsheets/d/1xCSMLMurBLSYvn5g3GmtinkDvmRdYxjQrysFr0IMtMQ/export?format=csv
+<?php 
 
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/1xCSMLMurBLSYvn5g3GmtinkDvmRdYxjQrysFr0IMtMQ/export?format=csv";
+
+if(!ini_set('default_socket_timeout',    15)) {
+    echo "<!-- unable to change socket timeout -->";
+
+    if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $spreadsheet_data[]=$data;
+            }
+        }
+        fclose($handle);
+}
+
+else
+    die("Problem reading csv");
 
 ?>
