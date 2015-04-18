d2dd50924f0919e30b48c382040611da39033ccc
Henry Mound <henrymound@gmail.com>
2015-04-18 16:25:09 +0000

    Added working algorithm to parse and count the number of items in a particular career cluster

--- processSheet.php
+++ processSheet.php
@@ -1,43 +1,87 @@
 
 <?php 
 
-// function readCSV($csvFile, $lineNumber){
+$CLUSTER_ARRAY = array(
     
+     "Agriculture, Food and Natural Resources",
+     "Architecture and Construction",
+     "Arts, A/V Technology and Communications",
+     "Business, Management and Administration",
+     "Education and Training",
+     "Finance",
+     "Government and Public Administration",
+     "Health Science",
+     "Hospitality and Tourism",
+     "Human Services",
+     "Information Technology",
+     "Law, Public Safety and Security",
+     "Manufacturing",
+     "Marketing, Sales and Service",
+     "Science, Technology, Engineering and Mathematics",
+     "Transportation, Distribution and Logistics"
     
-    
-// 	$file_handle = fopen($csvFile, 'r');
-// 	while (!feof($file_handle) ) {
-// 		$line_of_text[] = fgetcsv($file_handle, 1024);
-// 	}
-// 	fclose($file_handle);
-// //	return $line_of_text;
-//     return $line_of_text[$lineNumber];
+);
 
+
+    echo getNumberInCluster(5);
+    
+// if (($handle = fopen($csvFile, "r")) !== FALSE) {
+//     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+//         $num = count($data);
+//         echo "<p> $num fields in line $row: <br /></p>\n";
+//         $row++;
+//         for ($c=0; $c < $num; $c++) {
+//             echo $data[$c] . "<br />\n";
+//         }
+//     }
+//     fclose($handle);
 // }
 
 
-// Set path to CSV file
-$csvFile = 'https://docs.google.com/spreadsheets/d/1xCSMLMurBLSYvn5g3GmtinkDvmRdYxjQrysFr0IMtMQ/export?format=csv';
-
-// $csv = readCSV($csvFile, 1);
-
-//     echo '<pre>';
-//     print_r($csv);
-//     echo '</pre>';
+function getNumberInCluster($clusterNumber){
     
-
-$row = 1;
+    $csvFile = 'https://docs.google.com/spreadsheets/d/1xCSMLMurBLSYvn5g3GmtinkDvmRdYxjQrysFr0IMtMQ/export?format=csv';
+    $row = 1;
+    //  1'Agriculture, Food and Natural Resources'
+    //  2'Architecture and Construction'
+    //  3'Arts, A/V Technology and Communications'
+    //  4'Business, Management and Administration'
+    //  5'Education and Training'
+    //  6'Finance'
+    //  7'Government and Public Administration'
+    //  8'Health Science'
+    //  9'Hospitality and Tourism'   
+    //  10'Human Services'
+    //  11'Information Technology'
+    //  12'Law, Public Safety and Security'
+    //  13'Manufacturing'
+    //  14'Marketing, Sales and Service'
+    //  15'Science, Technology, Engineering and Mathematics'
+    //  16'Transportation, Distribution and Logistics'
     
-if (($handle = fopen($csvFile, "r")) !== FALSE) {
+    $clusterCounter = 0;
+    global $CLUSTER_ARRAY;
+
+    if (($handle = fopen($csvFile, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
+
+        //echo "<p> $num fields in line $row: <br /></p>\n";
+
+        if($data[4] == $CLUSTER_ARRAY[$clusterNumber])//$data[4] is the career cluster element
+        {
+            $clusterCounter++;
         }
+        
+        $row++;
     }
+    return $clusterCounter;
     fclose($handle);
+}
+
+    
+    
+    
 }
 
 ?>
