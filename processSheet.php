6161c3eab6663c6be1168ff41e1266d31a17b04d
Henry Mound <henrymound@gmail.com>
2015-04-18 00:53:31 +0000

    Updated to return line number of csv

--- processSheet.php
+++ processSheet.php
@@ -1,20 +1,25 @@
 
-<?php
-function readCSV($csvFile){
+<?php 
+
+function readCSV($csvFile, $lineNumber){
 	$file_handle = fopen($csvFile, 'r');
 	while (!feof($file_handle) ) {
 		$line_of_text[] = fgetcsv($file_handle, 1024);
 	}
 	fclose($file_handle);
-	return $line_of_text;
+//	return $line_of_text;
+    return $line_of_text[$lineNumber];
+
 }
 
 
 // Set path to CSV file
 $csvFile = 'https://docs.google.com/spreadsheets/d/1xCSMLMurBLSYvn5g3GmtinkDvmRdYxjQrysFr0IMtMQ/export?format=csv';
 
-$csv = readCSV($csvFile);
-echo '<pre>';
-print_r($csv);
-echo '</pre>';
+$csv = readCSV($csvFile, 1);
+
+    echo '<pre>';
+    print_r($csv);
+    echo '</pre>';
+
 ?>
