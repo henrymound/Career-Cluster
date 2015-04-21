d5d12a71095b30e6ad99f0c8a7e0e3114e1698ab
Henry Mound <henrymound@gmail.com>
2015-04-21 02:06:08 +0000

    Added info windows

--- processSheet.php
+++ processSheet.php
@@ -217,7 +217,7 @@ function getLatLangArray(){
     echo $ADDRESS_ARRAY[$i];
     
     for($i = 1; $i < getNumberOfCareers(); $i++){
-            array_push($LATLANGPAIR_ARRAY, geocode($ADDRESS_ARRAY[$i])[0].", ".geocode($ADDRESS_ARRAY[$i])[1]."<br />");
+            array_push($LATLANGPAIR_ARRAY, geocode($ADDRESS_ARRAY[$i])[0].", ".geocode($ADDRESS_ARRAY[$i])[1]."");
             //echo $LATLANGPAIR_ARRAY[$i-1];
     }
 
@@ -255,14 +255,50 @@ function displayMap(){
                   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);";
                   
                  for($i = 0; $i < getNumberOfCareers(); $i++){
+                     
+                      $contentString = "
+                          '<div id=\"content\">'+
+                          '<div id=\"siteNotice\">'+
+                          '</div>'+
+                          '<h1 id=\"firstHeading\" class=\"firstHeading\">Uluru</h1>'+
+                          '<div id=\"bodyContent\">'+
+                          '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                          'sandstone rock formation in the southern part of the '+
+                          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                          'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                          'Aboriginal people of the area. It has many springs, waterholes, '+
+                          'rock caves and ancient paintings. Uluru is listed as a World '+
+                          'Heritage Site.</p>'+
+                          '<p>Attribution: Uluru, <a href=\"https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194\">'+
+                          'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                          '(last visited June 22, 2009).</p>'+
+                          '</div>'+
+                          '</div>'
+                          
+                          ";
+                     
                      $htmlToPrint = $htmlToPrint."
-                          var marker = new google.maps.Marker({
+                     
+                            var infowindow = new google.maps.InfoWindow({
+                                  content: ".$contentString."
+                                });
+                     
+                             var marker".$i." = new google.maps.Marker({
                              position: new google.maps.LatLng(".$LATLANGPAIR_ARRAY[$i]."),
                              map: map,
                              title: 'Hello World!'
-                           });
+                                });
+                                
+                             google.maps.event.addListener(marker".$i.", 'click', function() {
+                                infowindow.open(map,marker".$i.");
+                                });
                         ";
+                        
                  }
+                 
                         
                 $htmlToPrint = $htmlToPrint."  
                 }
